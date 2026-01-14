358a5044ee034f29c0ba60fd2f5fcca56ce9c3e3
lake04 <wjdghksrla0409@gmail.com>
2026-01-14 20:12:50 +0900

    ADD | 기본적인 싱글톤,매니저 세팅

---
+++ UnityProject/2026programming/Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                if (instance == null)
+                {
+                    SetupInstance();
+                }
+            }
+            return instance;
+        }
+    }
+    public virtual void Awake()
+    {
+        RemoveDuplicates();
+    }
+
+    private static void SetupInstance()
+    {
+        instance = (T)FindAnyObjectByType(typeof(T));
+        if(instance == null)
+        {
+            GameObject go = new GameObject();
+            go.name = typeof(T).Name;
+            instance = go.AddComponent<T>();
+            DontDestroyOnLoad(go);
+        }
+    }
+
+    private void RemoveDuplicates()
+    {
+        if(instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
